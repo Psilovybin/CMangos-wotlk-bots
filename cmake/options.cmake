194042aa56ad87494abd7a53fbaeae33989b08ed
Chris Faulkner <me@chrisfaulkner.org>
2019-05-12 04:26:55 -0500

    Update options.cmake

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -6,7 +6,7 @@ option(BUILD_GAME_SERVER    "Build game server"                     ON)
 option(BUILD_LOGIN_SERVER   "Build login server"                    ON)
 option(BUILD_EXTRACTORS     "Build map/dbc/vmap/mmap extractors"    ON)
 option(BUILD_SCRIPTDEV      "Build ScriptDev. (OFF Speedup build)"  ON)
-#option(BUILD_PLAYERBOT      "Build Playerbot mod"                   OFF)
+option(BUILD_PLAYERBOT      "Build Player Bots"                     ON)
 option(BUILD_RECASTDEMOMOD  "Build map/vmap/mmap viewer"            OFF)
 option(BUILD_GIT_ID         "Build git_id"                          OFF)
 
